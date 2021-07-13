be79c6710ad5d8f205d31e07245a3e82f067d9db
ianwen0629 <ian.wen.2002@gmail.com>
2021-07-14 00:16:52 +0800

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -1,8 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from constants import Path
 
 class Firebase:
-    cred = credentials.Certificate('./service-account-private-key.json')
+    cred = credentials.Certificate(Path.RUBOT_FIREBASE_CERTIFICATE.value)
     firebase_admin.initialize_app(cred)
     db = firestore.client()
