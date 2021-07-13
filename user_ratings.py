cca95abc20a0819e61a1ce4339e637d97c66b167
ianwen0629 <ian.wen.2002@gmail.com>
2021-07-14 03:37:06 +0800

    restyled code so all lines have 79 characters max

--- user_ratings.py
+++ user_ratings.py
@@ -17,7 +17,8 @@ class User_Ratings(Firebase):
             return 6
         elif doc["rating"] == 1:
             return 0
-        rating = (doc["rating"] * doc["number-of-ratings"] + change) / (doc["number-of-ratings"] + 1)
+        rating = ((doc["rating"] * doc["number-of-ratings"] + change)
+                  / (doc["number-of-ratings"] + 1))
         
         doc_ref.set({
             u'number-of-ratings': (doc[u"number-of-ratings"] + 1),
@@ -43,11 +44,12 @@ class User_Ratings(Firebase):
 
     @classmethod
     async def check_time(cls, receiver, rater):
-        raters = cls.col_ref.document(str(receiver)).get().to_dict()["raters-history"]
+        doc_ref = cls.col_ref.document(str(receiver))
+        raters = doc_ref.get().to_dict()["raters-history"]
         try:
             time = raters[str(rater)]
         except KeyError:
-            cls.col_ref.document(str(receiver)).set({
+            doc_ref.set({
                 u'raters-history': {str(rater): datetime.min}
             }, merge=True)
             return None
