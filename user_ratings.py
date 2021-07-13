1902cf38d7c0a85207bbc6a61ed4e71ea3ea669b
ianwen0629 <ian.wen.2002@gmail.com>
2021-07-14 00:19:12 +0800

    Update user_ratings.py

--- user_ratings.py
+++ user_ratings.py
@@ -1,21 +1,16 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase import Firebase
 from datetime import datetime
 from datetime import timedelta
 from datetime import timezone
 
-class User_Ratings:
-    cred = credentials.Certificate('./service-account-private-key.json')
-    firebase_admin.initialize_app(cred)
-    db = firestore.client()
+class User_Ratings(Firebase):
+    col_ref = Firebase.db.collection(u'rubot-user-ratings')
 
     @classmethod
     async def rate(cls, receiver, rater, change):
         if await cls.check_time(receiver, rater) != None:
             return -1
-        col_ref = cls.db.collection(u'rubot-user-ratings')
-        doc_ref = col_ref.document(str(receiver))
+        doc_ref = cls.col_ref.document(str(receiver))
         
         doc = doc_ref.get().to_dict()
         if doc["rating"] == 5:
@@ -30,20 +25,16 @@ class User_Ratings:
             u'rating': rating
         }, merge=True)
 
-        '''docs = col_ref.stream()
-        for doc in docs:
-            print(f'{doc.id} => {doc.to_dict()}')'''
-
         return rating
 
     @classmethod
     async def check_user(cls, uid):
-        doc = cls.db.collection(u'rubot-user-ratings').document(str(uid)).get().to_dict()
+        doc = cls.col_ref.document(str(uid)).get().to_dict()
         return False if (doc == None) else True
 
     @classmethod
     async def init(cls, uid):
-        doc_ref = cls.db.collection(u'rubot-user-ratings').document(str(uid))
+        doc_ref = cls.col_ref.document(str(uid))
         doc_ref.set({
             u'number-of-ratings': 1,
             u'raters-history': {"creation": datetime.now(timezone.utc)},
@@ -52,11 +43,11 @@ class User_Ratings:
 
     @classmethod
     async def check_time(cls, receiver, rater):
-        raters = cls.db.collection(u'rubot-user-ratings').document(str(receiver)).get().to_dict()["raters-history"]
+        raters = cls.col_ref.document(str(receiver)).get().to_dict()["raters-history"]
         try:
             time = raters[str(rater)]
         except KeyError:
-            cls.db.collection(u'rubot-user-ratings').document(str(receiver)).set({
+            cls.col_ref.document(str(receiver)).set({
                 u'raters-history': {str(rater): datetime.min}
             }, merge=True)
             return None
